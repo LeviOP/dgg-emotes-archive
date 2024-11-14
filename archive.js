afd2ae4121f69c630332f6471e70c8bd7f3f6eac
Levi <35459268+LeviOP@users.noreply.github.com>
2024-11-14 11:24:33 -0600

    include flairs in archive

--- archive.js
+++ archive.js
@@ -1,34 +1,34 @@
 import * as fs from "node:fs/promises";
 import path from "node:path";
 
-const EMOTES_DIRECTORY = "emotes";
-
-fs.access(EMOTES_DIRECTORY).catch(() => {
-    fs.mkdir(EMOTES_DIRECTORY);
-}).finally(() => {
-    json();
-    css();
+["emotes", "flairs"].forEach((type) => {
+    fs.access(type).catch(() => {
+        fs.mkdir(type);
+    }).finally(() => {
+        json(type);
+        css(type);
+    });
 });
 
-async function json() {
-    const res = await fetch("https://cdn.destiny.gg/emotes/emotes.json");
+async function json(type) {
+    const res = await fetch(`https://cdn.destiny.gg/${type}/${type}.json`);
     const json = await res.clone().json();
-    images(json);
+    images(json, type);
     const text = await res.text();
-    fs.writeFile("./emotes.raw.json", text);
-    fs.writeFile("./emotes.json", JSON.stringify(json, null, 4));
+    fs.writeFile(`./${type}.raw.json`, text);
+    fs.writeFile(`./${type}.json`, JSON.stringify(json, null, 4));
 }
 
-async function images(emotes) {
-    for (const file of await fs.readdir("emotes")) await fs.unlink(path.join(EMOTES_DIRECTORY, file));
-    emotes.forEach((emote) => emote.image.forEach(async (image) => {
+async function images(list, type) {
+    for (const file of await fs.readdir(type)) await fs.unlink(path.join(type, file));
+    list.forEach((element) => element.image.forEach(async (image) => {
         const res = await fetch(image.url);
-        fs.writeFile(path.join(EMOTES_DIRECTORY, image.name), res.body);
+        fs.writeFile(path.join(type, image.name), res.body);
     }));
 }
 
-async function css() {
-    const res = await fetch("https://cdn.destiny.gg/emotes/emotes.css");
+async function css(type) {
+    const res = await fetch(`https://cdn.destiny.gg/${type}/${type}.css`);
     const text = await res.text();
-    fs.writeFile("./emotes.css", text);
+    fs.writeFile(`./${type}.css`, text);
 }
